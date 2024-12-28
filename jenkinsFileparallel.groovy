c89dd50a64bdae7a93f5477c9639bf61a962241e
VasukiTraning <vasukidayalu.vasantha@gmail.com>
2024-12-28 18:44:03 +0530

    Update jenkinsFileparallel.groovy

--- jenkinsFileparallel.groovy
+++ jenkinsFileparallel.groovy
@@ -42,7 +42,7 @@ pipeline {
         stage('Deploy to Tomcat') {
             steps {
                 sh '''
-                cp /home/ubuntu/workspace/pipelinejob1/target/StudentManagementApp-2.0-SNAPSHOT.war /usr/tomcat/tomcat10/webapps
+                cp /home/ubuntu/workspace/pipelinejob1/target/StudentManagementApp-1.0-SNAPSHOT.war /usr/tomcat/tomcat10/webapps
                 sudo /usr/tomcat/tomcat10/bin/shutdown.sh
                 sudo /usr/tomcat/tomcat10/bin/startup.sh
                 '''
